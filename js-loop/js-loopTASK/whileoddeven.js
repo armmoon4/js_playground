bede3877b5f99e0dd8e3e49cfc837f355aa7dba5
armmoon4 <rahman15-4343@diu.edu.bd>
2025-06-10 00:16:06 +0600

    update

---
+++ js-loop/js-loopTASK/whileoddeven.js
@@ -0,0 +1,24 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+/*programming hero*/
+
+let i = 61; 
+while(i <= 100){
+    console.log(i);
+    i = i + 2
+}
