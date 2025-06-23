52b225927c79d77a52e6139a9807d320a556a2db
armmoon4 <rahman15-4343@diu.edu.bd>
2025-06-23 22:36:30 +0600

    hw done

---
+++ jsProblems_part2/smallest.js
@@ -0,0 +1,14 @@
+const heights = [65, 66, 68, 72, 78, 60, 65, 66];
+
+function getMax(numbers){
+    let min = numbers[0];
+    for(const num of numbers){
+        if(num < min){
+            min = num;
+        }
+    }
+    return min;
+}
+
+const min = getMax(heights);
+console.log('min value is', min);
