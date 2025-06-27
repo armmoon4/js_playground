ae69e8a36ab446d7bb7712edd243e4f4711515ff
armmoon4 <rahman15-4343@diu.edu.bd>
2025-06-27 19:13:19 +0600

    createHTMLelemtent using js

---
+++ intregrate_js/tour_of_DOM/js/append.js
@@ -0,0 +1,45 @@
+//1. where to add
+const placeList = document.getElementById('places-list');
+
+//2. what to be added
+const li = document.createElement('li'); 
+li.innerText = 'pahartoli bon'; 
+
+//3. add the child 
+placeList.appendChild(li); 
+
+//1. where to add
+const mainContainer = document.getElementById('main-content');
+
+//2. what to be added 
+const section = document.createElement('section'); 
+const h1 = document.createElement('h1'); 
+h1.innerText = 'My food list'
+section.appendChild(h1); 
+
+const ul = document.createElement('ul'); 
+const li1 = document.createElement('li'); 
+li1.innerText = 'biriyani'; 
+ul.appendChild(li1); 
+
+
+const li2 = document.createElement('li'); 
+li2.innerText = 'pizza'; 
+ul.appendChild(li2); 
+
+ 
+const li3 = document.createElement('li'); 
+li3.innerText = 'burgar'; 
+ul.appendChild(li3); 
+
+
+const li4 = document.createElement('li'); 
+li4.innerText = 'chips'; 
+ul.appendChild(li4); 
+
+section.appendChild(ul);
+
+mainContainer.appendChild(section)
+
+//set innerHTML directly
+
