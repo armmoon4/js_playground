efc296698423401619ddeb9b642d762509ecad7b
armmoon4 <rahman15-4343@diu.edu.bd>
2025-07-20 15:36:40 +0600

    update 27-2

--- projects/Payoo-MFS/JS/login.js
+++ projects/Payoo-MFS/JS/login.js
@@ -1,4 +1,10 @@
+// step-1 set event handler
 document.getElementById('button-login')
-    .addEventListener('click', function () {
+    .addEventListener('click', function (event) {
+         //step-2 prevent default behaviour (reloding browser)
+        event.preventDefault();
         console.log('login click');
+        // step-3 get the phone number
+
     })
+
