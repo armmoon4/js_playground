f7d8e1799d9af28004e4017917b1b6ee1baedd36
armmoon4 <rahman15-4343@diu.edu.bd>
2025-07-23 19:26:00 +0600

    update handle login and redirect to homepage

--- projects/Payoo-MFS/JS/login.js
+++ projects/Payoo-MFS/JS/login.js
@@ -1,11 +1,37 @@
-// step-1 set event handler
-document.getElementById('button-login')
-    .addEventListener('click', function (event) {
-         //step-2 prevent default behaviour (reloding browser)
-        event.preventDefault();
-        console.log('login click');
-        // step-3 get the phone number
-        const phoneNumber = document.getElementById('phone-number').value;
-        console.log(phoneNumber)
-    })
+// // step-1 set event handler
+// document.getElementById('button-login')
+//     .addEventListener('click', function (event) {
+//          //step-2 prevent default behaviour (reloding browser)
+//         event.preventDefault();
+//         console.log('login click');
+//         // step-3 get the phone number
+//         const phoneNumber = document.getElementById('phone-number').value;
+//         const pinNumber = document.getElementById('pin-number').value; 
+//         console.log(phoneNumber , pinNumber);
+//     })
+//     //step 4 validate pin and phone
+//     // this is temporary . you should not do like this
+//     if(phoneNumber === '5' && pinNumber === '1234'){
+//         console.log('you are logged in');
+//         // allow user to the websitesss
+//     }
+//     else{
+//         alert('Wrong phone number or pin');
+//     }
 
+document.getElementById('button-login').addEventListener('click', function(event){
+    event.preventDefault();
+
+    // get phone number and pin number
+    const phoneNumber = document.getElementById('phone-number').value; 
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber , pinNumber);
+
+    // bad way to validate
+    if(phoneNumber === '5' && pinNumber ==='1234'){
+        console.log('your logged in'); 
+        window.location.href = '\home.html';
+    }else{
+        alert('Wrong phone number or pin.')
+    }
+});
