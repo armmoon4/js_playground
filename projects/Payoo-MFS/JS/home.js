9eeec844427b3a1f4a52362feb93423a72adc829
armmoon4 <rahman15-4343@diu.edu.bd>
2025-07-27 00:07:47 +0600

    add amount and pin number event handler done 27-5

--- projects/Payoo-MFS/JS/home.js
+++ projects/Payoo-MFS/JS/home.js
@@ -1,1 +1,13 @@
-console.log("hello")
+// add money to the account
+
+// step-1 add an event handler to the add money button inside the form
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    // prevent page reload after form from submit
+    event.preventDefault();
+    
+    // step 2 get money to be addded to the ac
+    const addMoneyinput = document.getElementById('input-add-money').value; 
+    // get pin number
+    const pinInput = document.getElementById('input-pin').value; 
+    console.log(addMoneyinput , pinInput); 
+});
