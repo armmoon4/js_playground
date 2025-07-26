ae12a157431b8c8a2d858f5c1b47fc1c1ec5f41a
armmoon4 <rahman15-4343@diu.edu.bd>
2025-07-27 00:13:16 +0600

    cursor bu

--- projects/Payoo-MFS/JS/home.js
+++ projects/Payoo-MFS/JS/home.js
@@ -1,13 +1,29 @@
 // add money to the account
 
+// s-1 add event handler
+// prevent page reload after from submit
+
+// s-2 get money to be added to the account balance
+// get the pin number
+
+// s-3 verify the pin number 
+
+
+
+
 // step-1 add an event handler to the add money button inside the form
 document.getElementById('btn-add-money').addEventListener('click', function(event){
     // prevent page reload after form from submit
     event.preventDefault();
     
+
     // step 2 get money to be addded to the ac
     const addMoneyinput = document.getElementById('input-add-money').value; 
     // get pin number
     const pinInput = document.getElementById('input-pin').value; 
     console.log(addMoneyinput , pinInput); 
+
+    // step 3 verify pin number
+    // wrong way to validate pin number
+    if(pinInput === '112344')
 });
