41ce0b98ca66efaad962c573c961f0573cfa6212
armmoon4 <rahman15-4343@diu.edu.bd>
2025-08-01 01:27:06 +0600

    recape add-money function

--- projects/payoo-recap/JS/addMoney.js
+++ projects/payoo-recap/JS/addMoney.js
@@ -1,6 +1,23 @@
-document.getElementById('add-money-btn').addEventListener('click', function(event){
+document.getElementById('add-money-btn').addEventListener('click', function (event) {
     event.preventDefault();
     const addMoney = document.getElementById('add-money-input').value;
-    const pinNumber = document.getElementById('add-money-pin').value; 
-    console.log(addMoney , pinNumber);
+    const addMoneyNumber = parseFloat(addMoney);
+    const pinNumber = document.getElementById('add-money-pin').value;
+    console.log(addMoney, pinNumber);
+
+    // wrong way to verify pin;;;
+
+    if (pinNumber === '123') {
+        const balance = document.getElementById('account-balance').innerText;
+        const balanceNumber = parseFloat(balance);
+        console.log(balance);
+        const newBalance = balanceNumber + addMoneyNumber ; 
+        document.getElementById('account-balance').innerText = newBalance;
+    } else {
+        alert('Wrong pin Number , Please try again!')
+    }
+
+
+
 })
+
