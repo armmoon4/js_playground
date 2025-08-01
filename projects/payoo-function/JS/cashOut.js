d05b365799f29ca9d61d3d2187ecca0f7565fc55
armmoon4 <rahman15-4343@diu.edu.bd>
2025-08-01 18:14:14 +0600

    cash out function added

--- projects/payoo-function/JS/cashOut.js
+++ projects/payoo-function/JS/cashOut.js
@@ -2,6 +2,17 @@ document.getElementById('cash-out-btn').addEventListener('click', function (even
     event.preventDefault();
     const cashOutAmount = getInputFieldValueById('cash-out-input');
     const pinNumber = getInputFieldValueById('cash-out-pin');
-    console.log("inside click handler", cashOutAmount, pinNumber);
+    // console.log("inside click handler", cashOutAmount, pinNumber);
+
+    if(pinNumber === 1234){
+        const acBalance = getTextFIeldValueById('account-balance');
+        // console.log("Current Balance:", acBalance);
+        const newBalance = acBalance - cashOutAmount;
+        // console.log("New Balance after cash out:", newBalance);
+        document.getElementById('account-balance').innerText = newBalance;
+    }else{
+        alert("Incorrect PIN. Please try again.");
+    }
 
 });
+
