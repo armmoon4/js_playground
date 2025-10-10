68cda2ed63d6a382a09b37c5bb1c55f47fa92fbd
armmoon4 <rahman15-4343@diu.edu.bd>
2025-10-11 00:37:37 +0600

    card added

--- intermediate_js/B11-A6-English-Janala-main/script/index.js
+++ intermediate_js/B11-A6-English-Janala-main/script/index.js
@@ -85,7 +85,34 @@ const displayWord = (words)=> {
         console.log(data);
     const wordCard = document.createElement("div"); 
     wordCard.innerHTML = `
-    <h2> ${data.meaning}  </h2>
+    <div class="bg-white rounded-lg shadow-lg p-8 w-full max-w-md hover:bg-blue-200 object-cover">
+            <h1 class="text-xl font-bold text-center text-gray-900 mb-4">
+                ${data.meaning}
+            </h1>
+            <p class="text-center text-gray-600 text-lg mb-6">
+                Meaning /Pronunciation
+            </p>
+            <p class="text-center text-sm font-semibold text-gray-800 mb-12">
+                "আগ্রহী / ইগার"
+            </p>
+            <div class="flex justify-between items-center">
+                <!-- Info Icon -->
+                <button class="bg-blue-100 hover:bg-blue-200 rounded-full p-3 transition-colors duration-200 hover:cursor-pointer">
+                    <svg class="w-6 h-6 text-blue-600" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd"
+                            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </button>
+                <button class="bg-blue-100 hover:bg-blue-200 rounded-full p-3 transition-colors duration-200 hover:cursor-pointer">
+                    <svg class="w-6 h-6 text-blue-600" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd"
+                            d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM14.657 2.929a1 1 0 011.414 0A9.972 9.972 0 0119 10a9.972 9.972 0 01-2.929 7.071 1 1 0 01-1.414-1.414A7.971 7.971 0 0017 10c0-2.21-.894-4.208-2.343-5.657a1 1 0 010-1.414zm-2.829 2.828a1 1 0 011.415 0A5.983 5.983 0 0115 10a5.984 5.984 0 01-1.757 4.243 1 1 0 01-1.415-1.415A3.984 3.984 0 0013 10a3.983 3.983 0 00-1.172-2.828 1 1 0 010-1.415z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </button>
+            </div>
+        </div>
     `;
 
     
