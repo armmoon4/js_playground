c2b85220372af46c3612c12816a860df5cc58dc3
armmoon4 <rahman15-4343@diu.edu.bd>
2025-10-10 21:33:32 +0600

    updated

--- intermediate_js/more_about_js/eventloop.js
+++ intermediate_js/more_about_js/eventloop.js
@@ -1,16 +1,21 @@
 function a() {
+    b();
     console.log("a");
 }
 
 function b() {
+    c();
     console.log("b");
 }
 
 function c() {
+    setTimeout(d, 0);
+    e();
     console.log("c");
 }
 
 function d() {
+    
     console.log("d");
 }
 
