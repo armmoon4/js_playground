60604e6636d3fa91fd6f8c8b1d89c4a4da1a2a17
armmoon4 <rahman15-4343@diu.edu.bd>
2025-10-12 19:19:50 +0600

    react setup and js debugging

---
+++ intermediate_js/JS Debugging/index.js
@@ -0,0 +1,30 @@
+// synatx error
+// unexpected token 
+// unexpected end
+// unexpected identifier
+// type error 
+
+
+// const a = 10; 
+//     a()
+
+// reference error
+
+// const sum = ()=> {
+//     const res = 10+ 1 ; 
+// }
+// console.log(res);
+
+const person = {
+    name: "john"
+}
+
+console.log(person)
+
+console.error("dkjfaklsdfgj")
+
+console.warn(person)
+
+console.table(person)
+
+// debugging steps >> 1 error identification >> error analysis >> fix and validation
